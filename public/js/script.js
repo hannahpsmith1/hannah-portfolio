105748decd4d52f243845329cedb948fd2347e32
Hannah Smith <hannahpsmith1@gmail.com>
2020-08-06 21:06:39 -0700

    figured out dynamic generation of partial pages

---
+++ public/js/script.js
@@ -0,0 +1,85 @@
+
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//       $("#div1").load("partial.html");
+//     });
+//   });
+
+//   $(document).ready(function(){
+//     $("button").click(function(){
+//       $("#div1").load("partial.html");
+//     });
+//   });
+
+
+
+// {/* <a id="projectsli">projects</a>  */}
+// $("#projectsli").click(function() { 
+//   $("#projects").load("projects.html"); 
+// });
+
+$(document).ready(function () {
+  $("home").click(function () {
+    console.log ("clicked");
+    $("#body").load("header.html");
+  });
+});
+
+
+$(document).ready(function () {
+  $("button").click(function () {
+    console.log ("clicked");
+    $("#textDiv").load("header.html");
+  });
+});
+
+
+$(document).ready(function () {
+  $("#myAnc2").click(function () {
+    $("#textTitle").load("partialproj.html");
+  });
+});
+
+$(document).ready(function () {
+  $("#myAnc").click(function () {
+    $("#div1").load("bulma.html");
+  });
+});
+
+$(document).ready(function () {
+  $("#myAnc2").click(function () {
+    $("#textDiv").load("partialproj.html");
+  });
+});
+
+$(document).ready(function () {
+  $("#myAnc3").click(function () {
+    $("#div1").load("footer.html");
+  });
+});
+
+// How to load projects portion on body.html
+$(document).ready(function () {
+  $("#myAnc4").click(function () {
+    $("#title").load("partialproj.html");
+  });
+});
+
+$(document).ready(function () {
+  $("#myAnc5").click(function () {
+    $("#title").load("partialproj.html");
+  });
+});
+
+$(document).ready(function () {
+  $("#myAnc4").click(function () {
+    $("#subtitle").load("header.html");
+  });
+});
+
+// $(document).ready(function(){
+//   $("#myAnc2").click(function() { 
+//     $("#heloo").load("partial.html");
+//   }); 
+//   }); 
